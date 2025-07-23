2ceb336227759698b494dec8a4a656950e521d5a
Aishani <aishaniv30@gmail.com>
2025-07-23 13:03:35 -0500

    Initial Commit

---
+++ GITPractice/GITPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GITPracticeApp.swift
+//  GITPractice
+//
+//  Created by Ranjan Verma on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct GITPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
